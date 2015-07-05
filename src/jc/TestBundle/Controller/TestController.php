88234cb770943cf31aed39b3b13a8b9673e9eaee
JC <jc@aequum.fr>
2015-07-05 12:58:22 +0200

    Fix design for BO

--- src/jc/TestBundle/Controller/TestController.php
+++ src/jc/TestBundle/Controller/TestController.php
@@ -7,6 +7,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class TestController extends Controller {
 
     public function testAction() {
-        return $this->render('jcTestBundle::test.html.twig');
+        return $this->render('jcTestBundle::test.html.twig', array("message" => "Test"));
+    }
+
+    public function path1Action() {
+        return $this->render('jcTestBundle::test.html.twig', array("message" => "Page for path 1"));
+    }
+
+    public function path2Action() {
+        return $this->render('jcTestBundle::test.html.twig', array("message" => "Page for path 2"));
+    }
+
+    public function path3Action() {
+        return $this->render('jcTestBundle::test.html.twig', array("message" => "Page for path 3"));
+    }
+
+    public function boAction() {
+        return $this->render('jcTestBundle::bo.html.twig');
     }
 }
