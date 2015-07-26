b7cca19e1f5a6df13566ca74794c1d6fa230d804
JC <jc@aequum.fr>
2015-07-26 16:04:47 +0200

    Improve pagination service

--- src/jc/ToolBundle/Service/PaginationService.php
+++ src/jc/ToolBundle/Service/PaginationService.php
@@ -4,6 +4,7 @@ namespace jc\ToolBundle\Service;
 
 use Doctrine\ORM\EntityManager;
 use jc\ToolBundle\Service\Model\PaginationInformation;
+use Doctrine\ORM\Query\Expr;
 
 class PaginationService {
 
@@ -22,21 +23,31 @@ class PaginationService {
      * @param String $entityName Name of entity we want to get pagiantion information.
      * @param String $orderField Optional field to sort result items.
      * @param String $orderValue Optional field to define sort order to use (if result items are sorted).
+     * @param array $filters Optional filters as array (field name as key, filter value as value).
      * @return PaginationInformation matching specified parameters.
      */
-    public function getPaginationInformation($page, $entityName, $orderField, $orderValue) {
+    public function getPaginationInformation($page, $entityName, $orderField = null, $orderValue = null, $filters = null) {
 
         try {
 
             // Count total item defined for specifed entity
-            $totalCount = $this->entityManager->createQuery('SELECT COUNT(e.id) FROM ' . $entityName . ' e')->getSingleScalarResult();
+            $queryCount = $this->entityManager->getRepository($entityName)->createQueryBuilder('e');
+            $queryCount->select('COUNT(e.id)');
+
+            // Add filters
+            $this->addConditionToQueryBuilder($queryCount, $filters);
+
+            $totalCount = $queryCount->getQuery()->getSingleScalarResult();
 
             // Prepare request to get all entities using optional sort field
             $queryBuilder = $this->entityManager->getRepository($entityName)->createQueryBuilder('e');
-            if (! empty($orderField) && ! empty($orderValue))
+            if (!empty($orderField) && !empty($orderValue))
                 $queryBuilder->addOrderBy('e.' . $orderField, $orderValue);
 
-                // Add pagination parameter
+            // Add filters
+            $this->addConditionToQueryBuilder($queryBuilder, $filters);
+
+            // Add pagination parameter
             $startIndex = ($page - 1) * $this->paginationCount;
             $queryBuilder->setFirstResult($startIndex)->setMaxResults($this->paginationCount);
 
@@ -50,6 +61,18 @@ class PaginationService {
         }
         catch (Exception $ex) {
             return null;
+        }
+    }
+
+    private function addConditionToQueryBuilder($queryBuilder, $filters) {
+
+        // Add filter if exist
+        if (!empty($filters)) {
+
+            foreach ($filters as $key => $value) {
+                $queryBuilder->andWhere('e.' . $key . ' = :' . $key)
+                ->setParameter($key, $value);
+            }
         }
     }
 }
