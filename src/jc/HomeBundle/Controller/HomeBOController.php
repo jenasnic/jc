33cbdda5f8592d3bb8ab723dc640495976c970f1
JC <jc@jc.com>
2016-10-20 11:48:37 +0200

    Update HomeBundle (using annotation)

--- src/jc/HomeBundle/Controller/HomeBOController.php
+++ src/jc/HomeBundle/Controller/HomeBOController.php
@@ -2,10 +2,14 @@
 
 namespace jc\HomeBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeBOController extends Controller {
 
+    /**
+     * @Route("/admin", name="jc_home_bo")
+     */
     public function adminAction() {
         return $this->render('jcHomeBundle:BO:admin.html.twig');
     }
--- src/jc/HomeBundle/Controller/HomeFOController.php
+++ src/jc/HomeBundle/Controller/HomeFOController.php
@@ -2,10 +2,14 @@
 
 namespace jc\HomeBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeFOController extends Controller {
 
+    /**
+     * @Route("/", name="jc_home_fo")
+     */
     public function homeAction() {
         return $this->render('jcHomeBundle:FO:home.html.twig');
     }
