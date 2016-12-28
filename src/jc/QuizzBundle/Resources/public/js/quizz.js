7be6ef8660e68c0c97e36a985b0f1ef330dc1d50
JC <jc@jc.com>
2016-12-28 17:24:31 +0100

    Improve BaseWebsiteBundle and QuizzBundle

--- src/jc/QuizzBundle/Resources/public/js/quizz.js
+++ src/jc/QuizzBundle/Resources/public/js/quizz.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     $(document).on('keypress', function(e) {
         if (e.keyCode == 13)
             processAnswer();
-    })
+    });
 });
 
 function processAnswer() {
@@ -36,28 +36,15 @@ function processAnswer() {
                 // In case of success => add response...
                 if (response.success) {
 
-                    // Check if reponse already found
-                    if ($('#quizz-movie-' + response.id).length && $('#quizz-picture-' + response.id).length) {
+                    addResponse(response.id, response.title, response.positionX, response.positionY, response.size);
 
-                        $('#quizz-popup').html('Déjà trouvé !!!');
-                        $('#quizz-popup').bPopup();
-                    }
-                    // Else => add response
-                    else {
-
-                        $('#quizz-popup').html('Bravo !!!');
-                        $('#quizz-popup').bPopup();
-                        $('#response').val('');
-                        addResponse(response.id, response.title, response.positionX, response.positionY, response.size);
-
-                        // Check if user has found all repsonses
-                        checkQuizzOver();
-                    }
+                    // Check if user has found all repsonses
+                    checkQuizzOver();
                 }
                 else {
 
                     $('#quizz-popup').html(response.message);
-                    $('#quizz-popup').bPopup();
+                    $('#quizz-popup').bPopup({autoClose: 1000});
                 }
             },
             error: function(msg) {
@@ -73,12 +60,12 @@ function addResponse(id, movie, positionX, positionY, pictureSize) {
     if ($('#quizz-movie-' + response.id).length && $('#quizz-picture-' + response.id).length) {
 
         $('#quizz-popup').html('Déjà trouvé !!!');
-        $('#quizz-popup').bPopup();
+        $('#quizz-popup').bPopup({autoClose: 1000});
     }
     else {
 
         $('#quizz-popup').html('Bravo !!!');
-        $('#quizz-popup').bPopup();
+        $('#quizz-popup').bPopup({autoClose: 1000});
         $('#response').val('');
 
 
@@ -99,7 +86,10 @@ function checkQuizzOver() {
     if ($('#response-list li').length == winningCount && $('#quizz div.quizz-picture').length == winningCount) {
 
         $('#quizz-answer').hide();
+        $('#quizz-info').hide();
         $('#quizz-popup').html('<h2>Félicitations !!!</h2>Vous avez trouvé les ' + winningCount + ' films.');
         $('#quizz-popup').bPopup();
     }
+    else
+        $('#response-count').html($('#quizz div.quizz-picture').length);
 }
